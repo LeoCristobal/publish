b3ef389f3292bb4410a44215726e6c1571ccf3a8
LeoCristobal <leocristobal108@gmail.com>
2025-09-07 14:27:30 +0800

    sample

--- test_db.php
+++ test_db.php
@@ -2,11 +2,14 @@
 $dbUrl = getenv('DATABASE_URL');
 $dbopts = parse_url($dbUrl);
 
-$dsn = "pgsql:host={$dbopts['host']};port={$dbopts['port']};dbname=" . ltrim($dbopts['path'],'/');
+$host = $dbopts['host'];
+$port = isset($dbopts['port']) ? $dbopts['port'] : 5432; // default 5432 kung walang port
 $user = $dbopts['user'];
 $pass = $dbopts['pass'];
+$dbname = ltrim($dbopts['path'], '/');
 
 try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;";
     $pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     echo "✅ Database connection successful!";
 } catch (PDOException $e) {
